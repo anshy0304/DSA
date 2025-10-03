2ae3062dd64665ab46f4a9a9a8a897712393a5c6
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2025-10-03 23:02:41 +0530

    Time: 0 ms (100%), Space: 42.9 MB (83.66%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int otherwise = 0;
+        for(int i=0;i<nums.length;i++) {
+            if(target == nums[i]) {
+                otherwise = i;
+                break;
+            }
+            if(target > nums[i]) {
+                otherwise++;
+            }
+        
+        }
+        return otherwise;
+    }
+}

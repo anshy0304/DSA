df693ef52fe464068adca3fee8e4940d20baf1a6
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2025-10-13 22:21:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (83.31%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,39 +15,21 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        // if(root  == null) return;
-        // if(root.left == null && root.right == null) return;
-        // TreeNode lefttree = root.left;
-        // TreeNode righttree = root.right;
-        // root.left = null;
-        // flatten(lefttree);
-        // flatten(righttree);
+        if(root  == null) return;
+        if(root.left == null && root.right == null) return;
+        TreeNode lefttree = root.left;
+        TreeNode righttree = root.right;
+        root.left = null;
+        flatten(lefttree);
+        flatten(righttree);
 
-        // root.right = lefttree;
-        // TreeNode temp = lefttree;
-        // while(temp!= null && temp.right!= null){
-        //     temp = temp.right;
-        // }   
-        // if(temp!=null)temp.right = righttree;
-        // else root.right  = righttree;
-        // return;
-
-
-        TreeNode curr = root;
-        
-        while(curr!=null) {
-            
-            if(curr.left != null) {
-                TreeNode pred = curr.left;
-                while(pred.right != null){
-                    pred = pred.right;
-                }
-                pred.right = curr.right;
-                curr.right = curr.left;
-                curr.left = null;
-            }
-            curr = curr.right;
-        }
-        
+        root.right = lefttree;
+        TreeNode temp = lefttree;
+        while(temp!= null && temp.right!= null){
+            temp = temp.right;
+        }   
+        if(temp!=null)temp.right = righttree;
+        else root.right  = righttree;
+        return;
          }
 }

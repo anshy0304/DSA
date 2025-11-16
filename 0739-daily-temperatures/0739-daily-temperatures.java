d248dec4a46f9e482bcce270f902393cf98a9f43
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2025-11-16 23:02:00 +0530

    Time: 77 ms (28.44%), Space: 106.8 MB (12.89%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -19,16 +19,35 @@
 // }
 
 
+// class Solution {
+//     public int[] dailyTemperatures(int[] nums) {
+//         Stack<Integer> st = new Stack<>();
+//         int n = nums.length;
+//         int[] ans= new int[nums.length];
+//         for(int i =0 ; i<n ; i++){
+//             while(!st.isEmpty() && nums[i]>nums[st.peek()]){
+//                 int prev= st.pop();
+//                 ans[prev]=i-prev;
+//             }
+//             st.push(i);
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
-    public int[] dailyTemperatures(int[] nums) {
-        Stack<Integer> st = new Stack<>();
-        int n = nums.length;
-        int[] ans= new int[nums.length];
-        for(int i =0 ; i<n ; i++){
-            while(!st.isEmpty() && nums[i]>nums[st.peek()]){
-                int prev= st.pop();
-                ans[prev]=i-prev;
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>(); // will store indices
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && temperatures[st.peek()] <= temperatures[i]) {
+                st.pop();
             }
+            ans[i] = st.isEmpty() ? 0 : st.peek() - i;
             st.push(i);
         }
         return ans;

11f4592a9ad1cc6a81165387ba3f000cfbae4c22
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-01-15 09:26:06 +0530

    Time: 2 ms (99.18%), Space: 118.9 MB (96.86%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,14 +1,43 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total=0,avail=0,ind=0;
-        for(int i=0;i<gas.length;i++){
-            total+=gas[i]-cost[i];
-            avail+=gas[i]-cost[i];
-            if(avail<0){
-                avail=0;
-                ind=i+1;
-            }
+       
+        // int n = gas.length;
+        // for(int i=0;i<gas.length;i++){
+            
+        //      int fuel = 0;
+        //     boolean possible = true;
+        //     for(int j =i;j<n+i;j++){
+        //         int in = j%n;
+        //         fuel += gas[in]-cost[in];
+        //         if(fuel<0){
+        //             possible = false;
+        //             break;
+        //         }
+
+        //     }
+        //     if(possible) return i;
+
+        // }
+        // return -1;
+
+
+        int totalGas = 0;
+        int totalCost = 0;
+        for(int i= 0;i<gas.length;i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
         }
-        return total<0?-1:ind;
+        if(totalGas <totalCost) return -1;
+        int fuel=0;int si=0;
+        for(int i=0;i<gas.length;i++){
+            fuel += gas[i]- cost[i];
+            if(fuel<0) {
+                fuel = 0;
+                si = i+1;
+            }
+        }   
+        return si;
     }
 }
+
+//linear queue drawbacks->after full it can never insert new element no matter if deleted started elements

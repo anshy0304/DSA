97dfc12b12236afbce6bfcae62f8adc37e3e67c4
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-03 09:47:00 +0530

    Time: 1 ms (95.49%), Space: 43 MB (73.51%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,17 +3,17 @@ class Solution {
         int n = word1.length() + word2.length();
         int i=0;
         int j=0;
-        String ans ="";
+        StringBuilder ans =new StringBuilder();
         while(n-->0){
             if(i<word1.length()){
-                ans+=word1.charAt(i);
+                ans.append(word1.charAt(i));
                 i++;
             }
             if(j<word2.length()){
-                ans+=word2.charAt(j);
+                ans.append(word2.charAt(j));
                 j++;
             }
         }
-        return ans;
+        return ans.toString();
     }
 }

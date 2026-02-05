4f071140674c6ec72e349178de01901459a27b8c
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-05 09:42:22 +0530

    Time: 5 ms (96.69%), Space: 44.3 MB (50.74%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,21 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int[] freq1 = new int[26];
-        for(char ch:s.toCharArray()){
-            freq1[ch-97]++;
-        }
-        int[] freq2 = new int[26];
-        for(char ch:t.toCharArray()){
-            freq2[ch-97]++;
-        }
-        
-        return Arrays.equals(freq1,freq2);
-    } 
+    
     public boolean checkInclusion(String s1, String s2) {
-        int n = s1.length();
-        for(int i=0;i<=s2.length()-n;i++){
-            String s = s2.substring(i,i+n);
-            if(isAnagram(s1,s)) return true;
+       if(s1.length()>s2.length()) return false;
+       int window = s1.length();
+       int freq1[] = new int[26];
+       int freq2[] = new int[26];
+       for(char ch:s1.toCharArray()){
+            freq1[ch-'a']++;
+       }
+        for(int i=0;i<window;i++){
+            freq2[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(freq1,freq2)) return true;
+        for(int i=window;i<s2.length();i++){
+            freq2[s2.charAt(i)-'a']++;
+            freq2[s2.charAt(i-window) -'a']--;
+            if(Arrays.equals(freq1,freq2)) return true;
         }
         return false;
     }

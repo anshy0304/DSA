cd9a0199f019f9eea8bfa7787f4278a668e21f8d
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-05 09:48:25 +0530

    Time: 9 ms (92.97%), Space: 47 MB (47.83%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -13,14 +13,25 @@ class Solution {
         return Arrays.equals(freq1,freq2);
     } 
     public List<Integer> findAnagrams(String s, String p) {
+        
         List<Integer> ans = new ArrayList<>();
-        int n = p.length();
-        for(int i=0;i<=s.length()-n;i++){
-            String sub = s.substring(i,i+n);
-            if(isAnagram(p,sub)){
-                ans.add(i);
-            }
+        if(p.length() > s.length()) return ans;
+        int window = p.length();
+        int freq1[] = new int[26];
+        int freq2[] = new int[26];
+        for(char ch:p.toCharArray()){
+            freq1[ch-'a']++;
         }
+        for(int i=0;i<window;i++){
+            freq2[s.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(freq1,freq2)) ans.add(0);
+        for(int i=window;i<s.length();i++){
+            freq2[s.charAt(i)-'a']++;
+            freq2[s.charAt(i-window) - 'a']--;
+            if(Arrays.equals(freq1,freq2)) ans.add(i-window+1);
+        }
+
         return ans;
     }
 }

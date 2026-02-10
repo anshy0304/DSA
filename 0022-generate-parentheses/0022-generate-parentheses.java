0d6bb093a15fc92e8a3d95392447547d7de53908
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-10 23:23:21 +0530

    Time: 2 ms (67.11%), Space: 45.1 MB (22.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,18 +1,23 @@
 class Solution {
-    private void helper(List<String> result,String current,int open,int close,int n) {
-        //base case
-        if(open == n && close == n) {
-            result.add(current);
+    public void helper(int n, List<String> ans,int open,int close,String per){
+        if(open == 0 && close == 0){
+            ans.add(per);
             return;
         }
-        if(open != n) helper(result,current +"(",open +1,close,n);
-
-        if(close != open) helper(result,current + ")",open,close + 1,n);
-     }
-    public List<String> generateParenthesis(int n) {
-        List<String> result = new ArrayList<>();
-        helper(result,"",0,0,n);
-        return result;
+        if(open != 0) helper(n,ans,open-1,close,per+'(');
+        if(open<close) {
+            
+            
+            helper(n,ans,open,close-1,per + ')');
+        }
         
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        int open = n-1;
+        int close = n;
+        String per = "(";
+        helper(n,ans,open,close,per);
+        return ans;
     }
 }

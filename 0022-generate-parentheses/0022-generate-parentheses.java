51ce37171223fcec8eac1938978eea185efb9be8
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-10 23:25:28 +0530

    Time: 1 ms (83.61%), Space: 44.3 MB (91.45%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,14 +1,16 @@
 class Solution {
-    public void helper(int n, List<String> ans,int open,int close,String per){
+    public void helper(int n, List<String> ans,int open,int close,StringBuilder per){
         if(open == 0 && close == 0){
-            ans.add(per);
+            ans.add(per.toString());
             return;
         }
-        if(open != 0) helper(n,ans,open-1,close,per+'(');
+        int len = per.length();
+        if(open != 0){ helper(n,ans,open-1,close,per.append('('));per.setLength(len);}
         if(open<close) {
             
             
-            helper(n,ans,open,close-1,per + ')');
+            helper(n,ans,open,close-1,per.append(')'));
+            per.setLength(len);
         }
         
     }
@@ -16,7 +18,7 @@ class Solution {
         List<String> ans = new ArrayList<>();
         int open = n-1;
         int close = n;
-        String per = "(";
+        StringBuilder per = new StringBuilder("(");
         helper(n,ans,open,close,per);
         return ans;
     }

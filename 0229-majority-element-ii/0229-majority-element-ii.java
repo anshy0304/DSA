066c746571313f418cd1a582f97d8717f08ad1fb
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-12 09:59:56 +0530

    Time: 20 ms (8.55%), Space: 58.5 MB (5.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,36 +1,13 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int c1 = 0,c2=0;
-        int count1 = 0,count2 = 0;
-        for(int n:nums){
-            if(c1 == n) {
-                count1++;
-            }
-            else if(c2 == n){
-                count2++;
-            }
-            else if(count1 == 0){
-                c1 = n;
-                count1=1;
-            }else if(count2==0){
-                c2 = n;
-                count2 = 1;
-            }
-            else {
-                count1--;
-                count2--;
-            }
-        }
-        count1 =0;
-        count2 = 0;
-        for(int n:nums){
-            if(n == c1)count1++;
-            else if(n == c2) count2++;
-        }
-        List<Integer> ans = new ArrayList<>();
-        if (count1 > nums.length / 3) ans.add(c1);
-        if (count2 > nums.length / 3) ans.add(c2);
-
-        return ans;
+       Arrays.sort(nums);
+       HashMap<Integer,Integer> map = new HashMap<>();
+       int n= nums.length;
+       for(int i:nums)map.put(i,map.getOrDefault(i,0)+1);
+       List<Integer> list = new ArrayList<>();
+       for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        if(entry.getValue() > n/3) list.add(entry.getKey());
+       }
+       return list;
     }
 }

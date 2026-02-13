52beb5fd44afc65a9212708924a7661dd9dadcb9
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-13 19:28:00 +0530

    Time: 10 ms (83.03%), Space: 46.8 MB (82.6%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,37 +1,22 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int[] freq1 = new int[26];
-        for(char ch:s.toCharArray()){
-            freq1[ch-97]++;
-        }
-        int[] freq2 = new int[26];
-        for(char ch:t.toCharArray()){
-            freq2[ch-97]++;
-        }
-        
-        return Arrays.equals(freq1,freq2);
-    } 
+    
     public List<Integer> findAnagrams(String s, String p) {
-        
         List<Integer> ans = new ArrayList<>();
+       int[] freq1 = new int[26];
+       int[] freq2 = new int[26];
+       for(char ch:p.toCharArray())freq1[ch-'a']++;
         if(p.length() > s.length()) return ans;
-        int window = p.length();
-        int freq1[] = new int[26];
-        int freq2[] = new int[26];
-        for(char ch:p.toCharArray()){
-            freq1[ch-'a']++;
-        }
-        for(int i=0;i<window;i++){
-            freq2[s.charAt(i) - 'a']++;
-        }
+       int window = p.length();
+       for(int i=0;i<window;i++){
+        freq2[s.charAt(i)-'a']++;
+       }
+        
         if(Arrays.equals(freq1,freq2)) ans.add(0);
-        for(int i=window;i<s.length();i++){
-            freq2[s.charAt(i)-'a']++;
-            freq2[s.charAt(i-window) - 'a']--;
-            if(Arrays.equals(freq1,freq2)) ans.add(i-window+1);
-        }
-
-        return ans;
+       for(int i=window;i<s.length();i++){
+        freq2[s.charAt(i)-'a']++;
+        freq2[s.charAt(i-window)-'a']--;
+        if(Arrays.equals(freq1,freq2)) ans.add(i-window+1);
+       }
+       return ans;
     }
 }

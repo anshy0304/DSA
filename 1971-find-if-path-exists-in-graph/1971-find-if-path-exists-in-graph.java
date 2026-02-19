3778985567423d1994deb327a66b86bf76989f9c
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-19 14:33:25 +0530

    Time: 166 ms (26.02%), Space: 327.9 MB (5.2%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,30 +1,27 @@
 class Solution {
 
-    public void dfs(ArrayList<ArrayList<Integer>> graph,int source,int destination,boolean visited[]){
-       visited[source] = true;
-       for(int i:graph.get(source)){
-            if(!visited[i]){
-                dfs(graph,i,destination,visited);
-            }
-
-       }
-    }
+   public void dfs(ArrayList<ArrayList<Integer>> graph,int source,int destination,boolean[] visited){
+        visited[source] = true;
+        for(int nbr:graph.get(source)){
+            if(!visited[nbr]){
+            dfs(graph,nbr,destination,visited);
+            }   
+        }
+   }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        boolean visited[] = new boolean[n];
-        for(int i=0;i<n;i++){
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i=0;i<edges.length;i++){
-            int a = edges[i][0];
-            int b = edges[i][1];
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-        dfs(graph,source,destination,visited);
-        return visited[destination];
+       ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+       for(int i=0;i<n;i++){
+        graph.add(new ArrayList<>());
+       }
+       for(int i=0;i<edges.length;i++){
+        int a = edges[i][0];
+        int b = edges[i][1];
+        graph.get(a).add(b);
+        graph.get(b).add(a);
+       }
+       boolean visited[] = new boolean[n];
+       dfs(graph,source,destination,visited);
+       return visited[destination];
     }
 }

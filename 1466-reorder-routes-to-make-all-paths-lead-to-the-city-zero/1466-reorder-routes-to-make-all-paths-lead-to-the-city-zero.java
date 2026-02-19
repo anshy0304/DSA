ed0c3376c5028c258a71affd73b7c6529f3f848a
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-19 14:56:42 +0530

    Time: 46 ms (55.11%), Space: 127.2 MB (34.9%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,37 +1,37 @@
 class Solution {
 
-    public void dfs(int node, List<List<Integer>> frwrd,List<List<Integer>> bkwrd,int[] ans, int[] visited) {
-                visited[node] = 1;
-                for(int nbr:frwrd.get(node)){
-                    if(visited[nbr] == 0){
-                        ans[0]+=1;
-                        dfs(nbr,frwrd,bkwrd,ans,visited);
-                    }
-                }
-                for(int nbr:bkwrd.get(node)){
-                    if(visited[nbr] == 0){
-                        dfs(nbr,frwrd,bkwrd,ans,visited);
-                    }
-                }
+    public void dfs(int node,List<List<Integer>> frwrd,List<List<Integer>> bkwrd,int[] ans,int[] visited){
+        visited[node] = 1;
+        for(int nbr:frwrd.get(node)){
+            if(visited[nbr] == 0) {
+                ans[0]+= 1;
+                dfs(nbr,frwrd,bkwrd,ans,visited);
+            }
+        }
+        for(int nbr:bkwrd.get(node)){
+            if(visited[nbr] == 0){
+                dfs(nbr,frwrd,bkwrd,ans,visited);
+            }
+        }
     }
+  
 
     public int minReorder(int n, int[][] connections) {
-        List<List<Integer>> frwrd = new ArrayList<>();
-        List<List<Integer>> bkwrd = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            frwrd.add(new ArrayList<>());
-            bkwrd.add(new ArrayList<>());
-        }
-        int visited[] = new int[n];
-        for (int i = 0; i < connections.length; i++) {
+       List<List<Integer>> frwrd = new ArrayList<>();
+       List<List<Integer>> bkwrd  = new ArrayList<>();
+       for(int i=0;i<n;i++){
+        frwrd.add(new ArrayList<>());
+        bkwrd.add(new ArrayList<>());
+       }
+       for(int i=0;i<connections.length;i++){
             int a = connections[i][0];
             int b = connections[i][1];
             frwrd.get(a).add(b);
             bkwrd.get(b).add(a);
-
-        }
-        int ans[] = new int[1];
-        dfs(0, frwrd, bkwrd, ans, visited);
-        return ans[0];
+       }
+       int[] visited = new int[n];
+       int[] ans = new int[1];
+       dfs(0,frwrd,bkwrd,ans,visited);
+       return ans[0];
     }
 }

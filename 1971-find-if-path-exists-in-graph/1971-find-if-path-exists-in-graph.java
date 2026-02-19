c02b17fab8bcee5e6ba75d8ebd86120e5de67b9a
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-02-19 15:07:16 +0530

    Time: 289 ms (10.21%), Space: 323.9 MB (5.2%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,6 +1,6 @@
 class Solution {
 
-   public void dfs(ArrayList<ArrayList<Integer>> graph,int source,int destination,boolean[] visited){
+   public void dfs(HashMap<Integer,ArrayList<Integer>> graph,int source,int destination,boolean[] visited){
         visited[source] = true;
         for(int nbr:graph.get(source)){
             if(!visited[nbr]){
@@ -10,9 +10,9 @@ class Solution {
    }
 
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-       ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+       HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
        for(int i=0;i<n;i++){
-        graph.add(new ArrayList<>());
+        graph.put(i,new ArrayList<>());
        }
        for(int i=0;i<edges.length;i++){
         int a = edges[i][0];
@@ -20,7 +20,7 @@ class Solution {
         graph.get(a).add(b);
         graph.get(b).add(a);
        }
-       boolean visited[] = new boolean[n];
+       boolean visited[]= new boolean[n];
        dfs(graph,source,destination,visited);
        return visited[destination];
     }

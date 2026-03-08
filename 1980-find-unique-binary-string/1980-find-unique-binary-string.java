059fc35b2e0149b189e632d787651f78b94f758d
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-03-08 18:26:30 +0530

    Time: 1109 ms (5.19%), Space: 47 MB (8.79%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    String finl ="";
+    public void permutation(StringBuilder ans,String[] nums,int n){
+        if(n == 0) {
+            boolean found = false;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i].equals(ans.toString())){ found = true;break;}
+            }
+            if(!found) finl = ans.toString();
+            return ;
+        }
+        int k = ans.length();
+        permutation(ans.append("0"),nums,n-1);
+        ans.setLength(k);
+        permutation(ans.append("1") ,nums,n-1);
+        ans.setLength(k);
+    }
+
+
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums[0].length();
+        StringBuilder ans = new StringBuilder();
+        
+        permutation(ans,nums,n);
+        return finl;
+    }
+}

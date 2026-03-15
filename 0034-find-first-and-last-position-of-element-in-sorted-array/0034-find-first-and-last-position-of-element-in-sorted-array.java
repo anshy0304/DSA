665165cec712b680ada8862aa665978e2f6df476
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-03-15 14:40:56 +0530

    Time: 0 ms (100%), Space: 48 MB (84.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,29 @@
 class Solution {
-    public int findFirst(int[] nums, int target){
-        int left = 0;
-        int right = nums.length -1;
-        int l = -1;
-        while(left<=right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target) {
-                l = mid;
-                right = mid -1;
-            }else if(target > nums[mid]) left = mid+1;
-            else right = mid -1;
-        }
-        return l;
-    }
-
-    public int findLast(int[] nums, int target){
-        int left = 0;
-        int right = nums.length - 1;
-        int r = -1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target) {
-                r = mid;
-                left = mid + 1;
-            }else if(target > nums[mid]) left = mid+1;
-            else right = mid -1;
-        }
-        return r;
-    }
+   
 
     public int[] searchRange(int[] nums, int target) {
-        int f = findFirst(nums,target);
-        int l = findLast(nums,target);
-        return new int[] {f,l};
+        int st = 0;
+        int end = nums.length-1;
+        int left = -1;
+        int right = -1;
+        while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid] == target){
+                left  = mid;
+                end = mid-1;
+            }else if(nums[mid] > target) end = mid-1;
+            else st = mid+1;
+        }
+        st = 0;
+        end = nums.length-1;
+         while(st<=end){
+            int mid = st + (end - st)/2;
+            if(nums[mid] == target){
+                right  = mid;
+                st = mid+1;
+            }else if(nums[mid] > target) end = mid-1;
+            else st = mid+1;
+        }
+        return new int[]{left,right};
     }
 }

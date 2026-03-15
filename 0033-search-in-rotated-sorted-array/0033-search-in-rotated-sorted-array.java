1c98362498632f7139416984a2d557854267c506
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-03-15 16:11:56 +0530

    Time: 3 ms (2.09%), Space: 43.9 MB (61.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,29 +1,37 @@
 class Solution {
 
-    public int rotate(int[] nums,int target, int si, int ei) {
-        if(si > ei) {
-            return -1;
+    public int peak(int[] arr){
+    int st = 0;
+    int end= arr.length-1;
+        while(st<=end){
+            if(arr[st] <= arr[end]) return st;
+            int mid = st+(end-st)/2;
+            int prev = (mid-1+arr.length)%arr.length;
+            int next =  (mid+1)%arr.length;
+            if(arr[mid] < arr[prev] && arr[mid] < arr[next]) return mid;
+            if(arr[mid] >= arr[st]) st = mid+1;
+            else end = mid-1;
         }
-        int mid = si + (ei - si)/2;
-        if(nums[mid] == target) {
-            return mid;
+            return 0;
+    }
+    
+    public int binary(int[] nums,int st ,int end,int target){
+        while(st<=end){
+            int mid = st+(end-st)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) end = mid -1;
+            else st = mid+1;
         }
-
-        if(nums[mid]>= nums[si]) {
-            if(target >= nums[si] && target <= nums[mid]) {
-                return rotate(nums,target,si,mid-1);
-            } else {
-                return rotate(nums,target,mid+1,ei);
-            }
-        } else {
-            if(target>=nums[mid] && target<=nums[ei]) {
-                return rotate(nums,target,mid+1,ei);
-            } else {
-                return rotate(nums,target,si,mid-1);
-            }
-        }
+        return -1;
     }
     public int search(int[] nums, int target) {
-        return rotate(nums,target,0,nums.length-1);
+        if(nums.length == 1) return (nums[0] == target)?0:-1;
+        int pk = peak(nums);
+        System.out.print(pk);
+        if(nums[pk] == target) return pk;
+        int left = binary(nums,0,pk-1,target);
+        if(left != -1) return left;
+        return binary(nums,pk+1,nums.length-1,target);
+        
     }
 }

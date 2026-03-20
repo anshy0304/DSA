e25effc9b49c15a19e66da9a4754826f7df6bd86
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-03-20 15:19:19 +0530

    Time: 7 ms (98.76%), Space: 46.6 MB (61.44%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,17 +10,14 @@
 class Solution {
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        if(root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
-        if(left == null){
-            return right;
-        }
-        if(right == null) {
-            return left;
-        }
-        return root;
+       if(root == null) return null;
+       if(root == p || root == q) return root;
+       TreeNode left =  lowestCommonAncestor(root.left,p,q);
+       TreeNode right = lowestCommonAncestor(root.right,p,q);
+       if(left != null && right != null) return root;
+       if(left != null) return left;
+       return right;
+       
     }
 
 }

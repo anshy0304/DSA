667d1a72b161ea90a321df2dddc2c418eb7261b7
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-03-29 09:23:19 +0530

    Time: 1 ms (100%), Space: 44.8 MB (11.22%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-        char[] a = new char[]{s1.charAt(0), s1.charAt(2)};
-        char[] b = new char[]{s2.charAt(0), s2.charAt(2)};
-        
-        char[] c = new char[]{s1.charAt(1), s1.charAt(3)};
-        char[] d = new char[]{s2.charAt(1), s2.charAt(3)};
-        
-        Arrays.sort(a);
-        Arrays.sort(b);
-        Arrays.sort(c);
-        Arrays.sort(d);
-        
-        return Arrays.equals(a, b) && Arrays.equals(c, d);
+        boolean even =
+            (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) ||
+            (s1.charAt(0) == s2.charAt(2) && s1.charAt(2) == s2.charAt(0));
+
+        boolean odd =
+            (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) ||
+            (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1));
+
+        return even && odd;
         
     }
 }

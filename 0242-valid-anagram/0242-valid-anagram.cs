d17790ea8bffc32b3a7501486b4d4d3e28005427
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-05-15 23:23:30 +0530

    Time: 1 ms (81.94%), Space: 44 MB (67.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,9 +1,13 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        int[] freq = new int[26];
-        int[] freq2 = new int[26];
-        foreach(char ch in s) freq[ch-'a']++;
-        foreach(char ch in t) freq2[ch-'a']++;
-        return freq.SequenceEqual(freq2);
-     }
+        int[] freqS = new int[26];
+        int[] freqT = new int[26];
+        foreach(var ch in s){
+            freqS[ch-'a']++;
+        }
+        foreach(var ch in t){
+            freqT[ch-'a']++;
+        }
+        return freqS.SequenceEqual(freqT);
+    }
 }

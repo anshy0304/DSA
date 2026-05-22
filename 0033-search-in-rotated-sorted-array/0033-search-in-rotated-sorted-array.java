526cc0f750b203231d12d80652520391e4574e90
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-05-22 12:53:00 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,37 +1,33 @@
 class Solution {
-
-    public int peak(int[] arr){
-    int st = 0;
-    int end= arr.length-1;
+    public int binary(int st,int end,int[] nums,int target){
         while(st<=end){
-            if(arr[st] <= arr[end]) return st;
-            int mid = st+(end-st)/2;
-            int prev = (mid-1+arr.length)%arr.length;
-            int next =  (mid+1)%arr.length;
-            if(arr[mid] < arr[prev] && arr[mid] < arr[next]) return mid;
-            if(arr[mid] >= arr[st]) st = mid+1;
-            else end = mid-1;
-        }
-            return 0;
-    }
-    
-    public int binary(int[] nums,int st ,int end,int target){
-        while(st<=end){
-            int mid = st+(end-st)/2;
+            int mid = st + (end-st)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target) end = mid -1;
+            else if(nums[mid] > target) end = mid-1;
             else st = mid+1;
         }
         return -1;
     }
+
     public int search(int[] nums, int target) {
-        if(nums.length == 1) return (nums[0] == target)?0:-1;
-        int pk = peak(nums);
-        System.out.print(pk);
-        if(nums[pk] == target) return pk;
-        int left = binary(nums,0,pk-1,target);
+        if(nums[0] < nums[nums.length-1]) return binary(0,nums.length-1, nums,target);
+        int pivot = 0;
+        int st = 0;
+        int end = nums.length-1;
+        int n = nums.length;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(nums[mid] == target) return mid;
+            int prev = (mid-1+n)%n;
+            int next = (mid+1)%n;
+            if(nums[mid] < nums[prev] && nums[mid] < nums[next]) {
+                pivot = mid;
+                break;
+            }else if(nums[mid] >= nums[0]) st = mid+1;
+            else end = mid -1 ;
+        }
+        int left = binary(0,pivot-1,nums,target);
         if(left != -1) return left;
-        return binary(nums,pk+1,nums.length-1,target);
-        
+        return binary(pivot,n-1,nums,target);
     }
 }

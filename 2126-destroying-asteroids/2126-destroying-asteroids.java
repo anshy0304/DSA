3151eb9602b12e26ddd26f381bf5ce84a7dab15f
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-06-01 00:37:33 +0530

    Time: 26 ms (81.52%), Space: 107.4 MB (47.81%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,13 +1,11 @@
 class Solution {
-    public boolean helper(int[] arr,long mass,int i){
-        if(i == arr.length){
-            return true;
-        }
-        if(mass < arr[i]) return false;
-        return helper(arr,mass + arr[i],i+1);
-    }
     public boolean asteroidsDestroyed(int mass, int[] a) {
         Arrays.sort(a);
-        return helper(a,(long)mass,0);
+        long sum = mass;
+        for(int i=0;i<a.length;i++){
+            if(sum < a[i]) return false;
+            sum += a[i];
+        }
+        return true;
     }
 }

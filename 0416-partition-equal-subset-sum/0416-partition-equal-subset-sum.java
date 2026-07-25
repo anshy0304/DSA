8a3196669ece3f4d1a09736bb62b783c2c9a6281
Ansh Yadav <168022373+anshy0304@users.noreply.github.com>
2026-07-25 20:27:45 +0530

    Time: 24 ms (90.61%), Space: 85.7 MB (7.3%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,29 +1,22 @@
 class Solution {
-
-    public boolean helper(int[] nums,int sum,int n,int[][] dp){
+    public boolean helper(int[] nums,int sum,int i,Integer[][] dp){
         if(sum == 0) return true;
-        if(n == 0) return false;
-        if(dp[n][sum] != -1) return dp[n][sum] == 1;
+        if(sum < 0 || i == nums.length) return false;
+        if(dp[sum][i] != null) return dp[sum][i] == 1;
         boolean res;
-        if(nums[n-1] <= sum){
-             res = helper(nums,sum-nums[n-1],n-1,dp) || helper(nums,sum,n-1,dp);
-        }else res = helper(nums,sum,n-1,dp);
-             dp[n][sum] = res ?1:0;
-        return res;
-        
+        if(sum >= nums[i]) {
+            res =  helper(nums,sum-nums[i],i+1,dp) || helper(nums,sum,i+1,dp);
+        }
+        else res = helper(nums,sum,i+1,dp);
+            dp[sum][i] = res ? 1 : 0;
+            return res;
     }
-
+    
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        int n = nums.length;
-        for (int num : nums)
-            sum += num;
-        if (sum % 2 != 0)
-            return false;
-        int dp[][] = new int[n + 1][(sum / 2) + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        return helper(nums, sum / 2, n, dp);
+        for(int n:nums) sum += n;
+        if(sum%2 != 0) return false;
+        Integer[][] dp = new Integer[(sum/2) + 1][nums.length];
+        return helper(nums,sum/2,0,dp);
     }
 }
